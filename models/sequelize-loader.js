356d0e29476d4f6e812a2db2967b40e03917768b
Qohey <kouhei_shimada@dwango.co.jp>
2021-02-16 13:19:32 +0900

    DBの環境変数変更

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -1,7 +1,7 @@
 'use strict';
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/quest_board'
+  process.env.HEROKU_POSTGRESQL_BLACK_URL || 'postgres://postgres:postgres@localhost/quest_board'
 );
 
 module.exports = {
